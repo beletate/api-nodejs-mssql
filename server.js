14b2f11a01d64aa58d64da52e6970e2cf44bbb54
Renato Camara <camarabox@gmail.com>
2021-12-08 14:57:11 -0300

    Update server.js
    update message

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ var app = require('./config/express');
 var port = process.env.PORT || 3000;
     
 app.listen(port, function(){
-    console.log('Server ON in Port' + port);
+    console.log('Server ON and running on Port' + port);
 })
